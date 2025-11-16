1336c0b5ae3c5b23e4b6501ae1402fbac258d5ae
EminYT777 <your_email@example.com>
2025-11-16 23:26:56 +0400

    add auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import AppRoutes from './components/AppRoutes'
+import { AuthProvider } from './components/AuthContext'
 
 function App() {
 
 
   return (
     <>
+    <AuthProvider>
       <AppRoutes />
+      </AuthProvider>
     </>
   )
 }
---
+++ src/components/AuthContext.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { createContext, useEffect } from "react";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    let navigate = UseNavigate();
+
+    useEffect(() => {
+        getUserAccount();
+    }, []);
+
+    function getUserAccount() {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            setUser(null);
+            return;
+        }
+
+        axios
+             .get(`${baseUrl}/auth/profile`, {
+                headers: {Authorization: `Bearer ${token}`},
+             })
+             .then((response) => setUser(response.data))
+             .catch((error) => {
+                console.error('Error fething user profile:', error);
+                localStorage.removeItem('token');
+                setUser(null);
+             });
+    }
+
+    function logoutUser() {
+        const token = localStorage.getItem('token');
+        if (token) {
+            axios 
+                .post(`${baseUrl}/auth/logout`, null, {
+                    headers: { Authorization: `Bearer ${token}` },
+                })
+                .then(() => {
+                    console.log('Logout successful!');
+                    localStorage.removeItem('token');
+                    setUser(null);
+                    navigate("/");
+                })
+                .catch(error => {
+                    console.error('Logout error:', error.response ? error.response.data : error.message);
+                    localStorage.removeItem('token');
+                    setUser(null);
+                    navigate("/");
+                });
+        } else {
+            console.log('No token found, user might already be logged out');
+            setUser(null);
+            navigate("/");
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, getUserAccount, logoutUser }}>
+             {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
